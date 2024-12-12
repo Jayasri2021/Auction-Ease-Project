7c0b47a78913474176d10c490e81af2f195cf260
pfw_Jayasri_Suresh_Vani <surej01@pfw.edu>
2024-12-12 12:00:00 -0500

    Header and Footer Update

---
+++ Auction-ease-frontend/src/components/NavbarComponent.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavbarComponent = ({ user, onLogout }) => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">AuctionEase</Link>
+      </div>
+      <div className="navbar-links">
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Signup</Link>
+        <Link to="/etl-dashboard">Add Bid ETL</Link>
+        <button onClick={onLogout} className="btn-logout">
+            Logout
+          </button>
+      </div>
+    </nav>
+  );
+};
+
+export default NavbarComponent;
